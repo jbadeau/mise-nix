90e1220d81fdd2742f1455a575ac434a27186d41
jbadeau <jose.badeau@juliusbaer.com>
2025-07-18 10:13:36 +0200

    feat: Only list versions compatible with user's platform

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
   name = "mise-nix",
-  version = "0.1.0",
+  version = "0.2.0",
   description = "A Mise backend plugin for installing tools using Nix.",
   author = "jose.badeau@gmail.com"
 }
