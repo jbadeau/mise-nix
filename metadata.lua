25f795f2baeab7ed5e2f9cde6236699502c2810f
jbadeau <jose.badeau@juliusbaer.com>
2025-08-09 07:54:19 +0200

    feat: Add vscode extension install support

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
   name = "mise-nix",
-  version = "0.8.0",
-  description = "A backend plugin for Mise that allows you to install and manage packages using Nix",
+  version = "0.11.0",
+  description = "A backend plugin for Mise that allows you to install and manage packages using Nix with VSCode extension support",
   author = "jbadeau"
 }
