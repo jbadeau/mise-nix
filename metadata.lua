e7cfcdd1476ba3ff97d2321cb39296a5a985c88c
jbadeau <jose.badeau@juliusbaer.com>
2025-08-11 10:10:32 +0200

    fix: Fixed vscode extension install

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
   name = "mise-nix",
-  version = "0.11.0",
+  version = "0.11.1",
   description = "A backend plugin for Mise that allows you to install and manage packages using Nix with VSCode extension support",
   author = "jbadeau"
 }
