e20f8b25acec1c717743273b8a59aa32ddd5628e
jbadeau <jose.badeau@juliusbaer.com>
2025-09-17 09:52:08 +0200

    feat: Support non semantic versions like Java 8u242-b08

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
   name = "mise-nix",
-  version = "0.14.0",
+  version = "0.14.1",
   description = "A Mise plugin that brings the power of the Nix ecosystem to your development workflow.",
   author = "jbadeau"
 }
