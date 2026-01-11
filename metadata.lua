a37f94a9ec7284e402e6e76653e8a91d536affe2
jbadeau <jose.badeau@juliusbaer.com>
2026-01-11 20:40:21 +0100

    chore: bump version to 0.16.0

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 PLUGIN = {
   name = "mise-nix",
-  version = "0.14.1",
+  version = "0.16.0",
   description = "A Mise plugin that brings the power of the Nix ecosystem to your development workflow.",
   author = "jbadeau"
 }
